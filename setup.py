48636609e6d0d742fefa3d2e594b3aa470f18f42
Abdallah Ibrahim <abdallahelghonamy22@gmail.com>
2025-08-15 02:25:38 +0300

    setting up the packages

---
+++ setup.py
@@ -0,0 +1,26 @@
+# used to build our application as a package that could be reusable
+
+from setuptools import find_packages , setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(filePath:str)->List[str]:
+    # will return the list requirements
+    requirements = List[str]
+    with open(filePath) as file_obj:
+        requirements= file_obj.readlines()
+        requirements= [ req.replace('\n' , '') for req in requirements]
+        if HYPEN_E_DOT in requirements: 
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name= 'first_project',
+    version='0.0.1',
+    author='elgho',
+    packages=find_packages(), # it will look to the folders that contains __init__.py
+    # as it will contain the imported packages and so
+    install_requires=get_requirements('requirements.txt') # the needed packages to be installed
+
+)
