6188f4e566ca2034575af3b144acb85f66c69e33
Abdallah Ibrahim <abdallahelghonamy22@gmail.com>
2025-08-16 22:05:04 +0300

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_object(file_path:str , obj):
     try:
@@ -28,20 +28,16 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = RandomizedSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
+            # assigning the best parameters to the assigned model
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-
-            y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
 
